2a06e158de30d684078b88066ab6ea128eff0ab4
dwanhb <dennis.wanhb@gmail.com>
2025-12-07 12:31:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from “@vitejs/plugin-react”;
 import path from “path”;
 
 export default defineConfig({
-  base: '/hndy-teaser/'
 base: ‘/hndy-teaser/’, // This is critical for GitHub Pages
 plugins: [react()],
 resolve: {
